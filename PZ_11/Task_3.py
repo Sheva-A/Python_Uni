354ea35138f52f9d1f6d8ee5e9c8c49a748d8e9d
Anna Shevchenko <a.sheva.second@gmail.com>
2025-05-03 12:48:25 +0300

    PZ-11 completed

---
+++ PZ_11/Task_3.py
@@ -0,0 +1,13 @@
+"""
+Пошук чисел, що повторюються у списку.
+Використовуються тільки цикли та умовні оператори.
+"""
+
+numbers = [1, 2, 3, 4, 3, 2, 5, 6, 5, 7]
+duplicates = []
+
+for num in numbers:
+    if numbers.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
+
+print("Повторювані числа:", duplicates)
